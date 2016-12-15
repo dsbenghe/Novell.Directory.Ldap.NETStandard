5a54fb59f190bf14fa96dcb4cacce20e66c44b8c
dsbenghe <dsbenghe@gmail.com>
2016-12-15 20:15:49 +1100

    search test

---
+++ test/Novell.Directory.Ldap.NETStandard.FunctionalTests/SearchTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Novell.Directory.Ldap.NETStandard.FunctionalTests.Helpers;
+using Xunit;
+
+namespace Novell.Directory.Ldap.NETStandard.FunctionalTests
+{
+    public class SearchTests
+    {
+        [Fact]
+        public void Can_Search_ByCn()
+        {
+            const int noOfEntries = 10;
+            var ldapEntries = Enumerable.Range(1, noOfEntries).Select(x => LdapOps.AddEntry()).ToList();
+            var ldapEntry = ldapEntries[new Random().Next() % noOfEntries];
+            TestHelper.WithAuthenticatedLdapConnection(
+                ldapConnection =>
+                {
+                    var lsc = ldapConnection.Search(TestsConfig.LdapServer.BaseDn, LdapConnection.SCOPE_SUB, "cn=" + ldapEntry.getAttribute("cn").StringValue, null, false);
+                    var entries = new List<LdapEntry>();
+                    while (lsc.hasMore())
+                    {
+                        entries.Add(lsc.next());
+                    }
+                    Assert.Equal(1, entries.Count);
+                    ldapEntry.AssertSameAs(entries[0]);
+                });
+        }
+    }
+}
