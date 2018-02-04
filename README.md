41477654d75cf8c0f89f52fd1b8ad71e5e36268a
dsbenghe <dsbenghe@gmail.com>
2018-02-04 16:11:38 +0100

    small text changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# LDAP client library for .NET Standard 1.3
+# .NET Standard LDAP client library
 
-Supported on the .NET Standard 1.3 (https://docs.microsoft.com/en-us/dotnet/articles/standard/library) compatible .NET runtimes: .NET Core, .NET Framework 4.6, Universal Windows Platform, Xamarin.
+Supported on the .NET Standard - minimum required is 1.3 - (https://docs.microsoft.com/en-us/dotnet/articles/standard/library) compatible .NET runtimes: .NET Core, .NET Framework 4.6, Universal Windows Platform, Xamarin.
 
 It works with any LDAP protocol compatible directory server (including Microsoft Active Directory).
 
--- src/Novell.Directory.Ldap.NETStandard/Properties/AssemblyInfo.cs
+++ src/Novell.Directory.Ldap.NETStandard/Properties/AssemblyInfo.cs
@@ -41,7 +41,7 @@ using System.Reflection;
 [assembly: AssemblyTitle("C# LDAP")]
 [assembly: AssemblyDescription("Novell.Directory.Ldap")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Novell, Inc")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright(" (C) 2003 Novell, Inc, 2016 dsbenghe")]
 [assembly: AssemblyTrademark("")]
@@ -59,7 +59,7 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.3.0")]
+[assembly: AssemblyVersion("3.0.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
