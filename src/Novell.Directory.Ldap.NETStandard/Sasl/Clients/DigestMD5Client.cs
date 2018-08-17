c26da0463013ed47f9e0522b17404649b084fdde
Michael Stum <git@stum.de>
2018-08-17 09:10:37 -0400

    Stub DigestMD5Client class

---
+++ src/Novell.Directory.Ldap.NETStandard/Sasl/Clients/DigestMD5Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Novell.Directory.Ldap.Sasl.Clients
+{
+#pragma warning disable CA5351 // Do Not Use Broken Cryptographic Algorithms
+    /// <summary>
+    /// Digest Authentication as defined in RFC 2831:
+    /// https://tools.ietf.org/html/rfc2831
+    /// 
+    /// RFC 6331 marks DIGEST-MD5 as obsolete/historic:
+    /// https://tools.ietf.org/html/rfc6331
+    /// 
+    /// However, it is still in use.
+    /// </summary>
+    public class DigestMD5Client : BaseSaslClient
+    {
+        public DigestMD5Client(string protocol, string serverName, Hashtable props) : base(protocol, serverName, props)
+        {
+        }
+
+        public override string MechanismName => SaslConstants.Mechanism.DigestMd5;
+
+        public override bool HasInitialResponse => throw new NotImplementedException();
+
+        public override bool IsComplete => throw new NotImplementedException();
+
+        public override byte[] EvaluateChallenge(byte[] challenge)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
