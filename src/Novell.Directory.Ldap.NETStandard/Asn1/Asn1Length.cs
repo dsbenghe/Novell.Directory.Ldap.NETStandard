56106cd0142638e4b373db9aed846cb26ceec1ad
Michael Stum <git@stum.de>
2018-08-18 10:44:21 -0400

    DRY: Call Reset(Stream) from ctor

--- src/Novell.Directory.Ldap.NETStandard/Asn1/Asn1Length.cs
+++ src/Novell.Directory.Ldap.NETStandard/Asn1/Asn1Length.cs
@@ -41,12 +41,6 @@ namespace Novell.Directory.Ldap.Asn1
     /// </summary>
     public class Asn1Length
     {
-        /* Private variables
-        */
-
-        /* Constructors for Asn1Length
-        */
-
         /// <summary> Constructs an empty Asn1Length.  Values are added by calling reset.</summary>
         public Asn1Length()
         {
@@ -67,31 +61,7 @@ namespace Novell.Directory.Ldap.Asn1
         /// </param>
         public Asn1Length(Stream inRenamed)
         {
-            var r = inRenamed.ReadByte();
-            EncodedLength++;
-            if (r == 0x80)
-            {
-                Length = -1;
-            }
-            else if (r < 0x80)
-            {
-                Length = r;
-            }
-            else
-            {
-                Length = 0;
-                for (r = r & 0x7F; r > 0; r--)
-                {
-                    var part = inRenamed.ReadByte();
-                    EncodedLength++;
-                    if (part < 0)
-                    {
-                        throw new EndOfStreamException("BERDecoder: decode: EOF in Asn1Length");
-                    }
-
-                    Length = (Length << 8) + part;
-                }
-            }
+            Reset(inRenamed);
         }
 
         /// <summary> Returns the length of this Asn1Length.</summary>
