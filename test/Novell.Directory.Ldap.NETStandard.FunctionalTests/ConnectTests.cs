6eacb9011e60d4cabb0cf3b18f48c56ea2b7659c
dsbenghe <dsbenghe@gmail.com>
2020-05-20 19:32:10 +0200

    more connect tests

--- test/Novell.Directory.Ldap.NETStandard.FunctionalTests/ConnectTests.cs
+++ test/Novell.Directory.Ldap.NETStandard.FunctionalTests/ConnectTests.cs
@@ -8,12 +8,38 @@ namespace Novell.Directory.Ldap.NETStandard.FunctionalTests
     public class ConnectTests
     {
         [Fact]
-        public async Task Connect_Works()
+        public async Task Connect_when_correct_credentials_is_successful()
         {
             await TestHelper.WithLdapConnectionAsync(
                 async ldapConnection =>
                 {
                     await ldapConnection.BindAsync(TestsConfig.LdapServer.RootUserDn, TestsConfig.LdapServer.RootUserPassword);
+                });
+        }
+
+        [Fact]
+        public async Task Connect_when_empty_dn_throws_exception()
+        {
+            await TestHelper.WithLdapConnectionAsync(
+                async ldapConnection =>
+                {
+                    await Assert.ThrowsAsync<LdapException>(
+                        async () => await ldapConnection.BindAsync(
+                            string.Empty, 
+                            TestsConfig.LdapServer.RootUserPassword));
+                });
+        }
+
+        [Fact]
+        public async Task Connect_when_wrong_password_throws_exception()
+        {
+            await TestHelper.WithLdapConnectionAsync(
+                async ldapConnection =>
+                {
+                    await Assert.ThrowsAsync<LdapException>(
+                        async () => await ldapConnection.BindAsync(
+                            TestsConfig.LdapServer.RootUserDn, 
+                            TestsConfig.LdapServer.RootUserPassword + "1"));
                 });
         }
 
@@ -58,7 +84,7 @@ namespace Novell.Directory.Ldap.NETStandard.FunctionalTests
                 }, false, true);
         }
 
-        [Fact(Skip = "This randomly fails")]
+        [Fact]
         public async Task Connect_WithBindAfterStartTlsAndRestoreNonTls_Works()
         {
             await TestHelper.WithLdapConnectionAsync(
