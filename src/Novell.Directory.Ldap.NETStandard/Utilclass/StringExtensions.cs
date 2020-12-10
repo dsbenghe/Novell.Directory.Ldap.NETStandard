02004b49b6f2fd65897276234b7b4eebf65f2a0b
isnogud <wZjtEZPkRnUmxJ>
2020-12-10 21:13:35 +0100

    Add new argument check

--- src/Novell.Directory.Ldap.NETStandard/Utilclass/StringExtensions.cs
+++ src/Novell.Directory.Ldap.NETStandard/Utilclass/StringExtensions.cs
@@ -9,8 +9,12 @@ namespace Novell.Directory.Ldap.Utilclass
         /// </summary>
         public static bool StartsWithStringAtOffset(this string baseString, string value, int offset)
         {
-            if (value == null) throw new ArgumentNullException(nameof(value));
-            if (offset < 0) throw new ArgumentOutOfRangeException(nameof(offset));
+            if (value == null) 
+                throw new ArgumentNullException(nameof(value));
+            if (offset < 0) 
+                throw new ArgumentOutOfRangeException(nameof(offset));
+            if (offset > baseString.Length) 
+                throw new ArgumentOutOfRangeException(nameof(offset), "offset cannot be larger than length of string");
 
             if (offset + value.Length > baseString.Length)
             {
