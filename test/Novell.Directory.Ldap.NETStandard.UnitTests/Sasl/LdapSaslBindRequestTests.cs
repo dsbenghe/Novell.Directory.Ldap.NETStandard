22b1c80b952f02412405d30a0d220fa9a2733d93
dsbenghe <dsbenghe@gmail.com>
2020-12-29 10:55:41 +0100

    Fix resharper incorrect improvement

---
+++ test/Novell.Directory.Ldap.NETStandard.UnitTests/Sasl/LdapSaslBindRequestTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace Novell.Directory.Ldap.NETStandard.UnitTests.Sasl
+{
+    public class LdapSaslBindRequestTests
+    {
+        [Fact]
+        public void LdapSaslBindRequest_can_create_string_for_debugging()
+        {
+            var mechanism = Guid.NewGuid().ToString();
+            var ldapSaslBindRequest = new LdapSaslBindRequest(3, mechanism, null);
+
+            var str = ldapSaslBindRequest.ToString();
+
+            Assert.Contains(mechanism, str);
+        }
+    }
+}
