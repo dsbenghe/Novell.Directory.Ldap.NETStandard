070e8f49abc2ee978eb43771909e0a744e77a8d8
James Cracknell <james.h.cracknell@gmail.com>
2022-03-11 16:51:01 -0500

    Remove dependency on System.Linq.Async (#203)
    Added ToListAsync implementation to eliminate dependency on
    System.Linq.Async, which provided System.Linq.AsyncEnumerable.ToListAsync.
    
    System.Linq.Async introduces extension methods to a default (or even
    mandatory) namespace which conflict with other implementations, see:
    https://github.com/dotnet/reactive/issues/1057

---
+++ src/Novell.Directory.Ldap.NETStandard/Utilclass/AsyncEnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Novell.Directory.Ldap.Utilclass
+{
+    public static class AsyncEnumerableExtensions
+    {
+        /// <summary>
+        /// Asynchronously materializes the subject <see cref="IAsyncEnumerable{T}"/> into a list.
+        /// </summary>
+        public static async Task<List<T>> ToListAsync<T>(this IAsyncEnumerable<T> enumerable, CancellationToken cancellationToken = default)
+        {
+            var list = new List<T>();
+            await foreach (var element in enumerable.WithCancellation(cancellationToken))
+            {
+                list.Add(element);
+            }
+
+            return list;
+        }
+    }
+}
--- test/Novell.Directory.Ldap.NETStandard.FunctionalTests/SearchTests.cs
+++ test/Novell.Directory.Ldap.NETStandard.FunctionalTests/SearchTests.cs
@@ -1,4 +1,5 @@
 ﻿using Novell.Directory.Ldap.NETStandard.FunctionalTests.Helpers;
+using Novell.Directory.Ldap.Utilclass;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
